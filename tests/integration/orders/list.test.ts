330cccb1afb13c9dfe1790f1e3b965da35ba8097
Juliana Ando <juandocezare@gmail.com>
2023-06-28 17:29:05 -0300

    Testes 100%

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,19 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import OrderModel from '../../../src/database/models/order.model';
+import orderMock from '../../mocks/orders.mocks';
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it('lista todos os produtos', async function () {
+    const mockProducts = OrderModel.build(orderMock.validOrder);
+    sinon.stub(OrderModel, 'findAll').resolves([mockProducts]);
+    const httpResponse = await chai.request(app).get('/orders');
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.be.deep.equal([orderMock.validOrder]);
+  });
 });
---
+++ tests/mocks/orders.mocks.ts
@@ -0,0 +1,8 @@
+const validOrder = {
+    id: 1,
+    userId: 1,
+  };
+
+export default {
+  validOrder,
+};
