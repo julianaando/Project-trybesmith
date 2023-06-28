5e87713c7e5e5747ff24d536159570adbaa5f7fa
Juliana Ando <juandocezare@gmail.com>
2023-06-28 17:18:01 -0300

    Req 03 done

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,14 +1,24 @@
-import OrderModel, {
-  OrderSequelizeModel,
-} from '../database/models/order.model';
+import { literal } from 'sequelize';
+import OrderModel, { OrderSequelizeModel } from '../database/models/order.model';
 import { ServiceResponse } from '../types/ServiceResponse';
+import ProductModel from '../database/models/product.model';
 
 async function listAll(): Promise<ServiceResponse<OrderSequelizeModel[]>> {
-  const orders = await OrderModel.findAll();
-  return {
-    status: 'SUCCESSFUL',
-    data: orders,
-  };
+  const orders = await OrderModel.findAll({
+    attributes: [
+      'id',
+      'userId',
+      [literal('JSON_ARRAYAGG(productIds.id)'), 'productIds'],
+    ],
+    include: [{
+      model: ProductModel,
+      as: 'productIds',
+      attributes: [],
+    }],
+    group: ['Order.id'],
+    raw: true,
+  });
+  return { status: 'SUCCESSFUL', data: orders };
 }
 
 export default {
